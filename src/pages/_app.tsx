98c88f32c36f1287f6e39847c6fab29404ec4439
Manuel Bichler <mbichler@cemm.at>
2023-01-19 20:37:03 +0100

    add app back

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,15 @@
 import { useEffect, useRef, useState } from 'react'
 
-import { QueryClient } from '@tanstack/react-query'
+import {
+  Hydrate,
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query'
 
 import type { AppProps } from 'next/app'
 
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
 import '@/styles/tailwind.css'
 import 'focus-visible'
 import { trpc } from '../utils/trpc'
@@ -28,23 +34,22 @@ function MyApp({
   const [queryClient] = useState(() => new QueryClient())
 
   return (
-    <div className="mt-20 flex justify-center align-middle">{':)'}</div>
-    // <QueryClientProvider client={queryClient}>
-    //   <Hydrate state={pageProps.dehydratedState}>
-    //     <div className="fixed inset-0 flex justify-center sm:px-8">
-    //       <div className="flex w-full max-w-7xl lg:px-8">
-    //         <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
-    //       </div>
-    //     </div>
-    //     <div className="relative">
-    //       <Header />
-    //       <main>
-    //         <Component previousPathname={previousPathname} {...pageProps} />
-    //       </main>
-    //       <Footer />
-    //     </div>
-    //   </Hydrate>
-    // </QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      <Hydrate state={pageProps.dehydratedState}>
+        <div className="fixed inset-0 flex justify-center sm:px-8">
+          <div className="flex w-full max-w-7xl lg:px-8">
+            <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
+          </div>
+        </div>
+        <div className="relative">
+          <Header />
+          <main>
+            <Component previousPathname={previousPathname} {...pageProps} />
+          </main>
+          <Footer />
+        </div>
+      </Hydrate>
+    </QueryClientProvider>
   )
 }
 
