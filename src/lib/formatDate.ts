28b6041daec1d6eb04e5dc00e22188b72e691088
Manuel Bichler <mbichler@cemm.at>
2023-03-03 16:06:20 +0100

    fixed formate date function

--- src/lib/formatDate.ts
+++ src/lib/formatDate.ts
@@ -1,14 +1,16 @@
-export function formatDate(dateString: string) {
+export function formatDate(dateString?: string): string {
   if (!dateString) return ''
-  if (dateString.charAt(dateString.length - 1) === 'Z')
-    return new Date(dateString).toLocaleDateString('en-GB', {
+  if (/^\d{4}-\d{2}-\d{2}$/.test(dateString)) {
+    new Date(`${dateString}T00:00:00Z`)?.toLocaleDateString('en-GB', {
       day: 'numeric',
       month: 'long',
       year: 'numeric',
       timeZone: 'UTC',
     })
-
-  return new Date(`${dateString}T00:00:00Z`).toLocaleDateString('en-GB', {
+  }
+  const date = new Date(dateString)
+  if (isNaN(date.getTime())) return ''
+  return date.toLocaleDateString('en-GB', {
     day: 'numeric',
     month: 'long',
     year: 'numeric',
