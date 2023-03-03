c0036d413ba616e3a07e0199b1c50cf18c42e452
Manuel Bichler <mbichler@cemm.at>
2023-03-03 16:33:27 +0100

    let's seeee

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -1,6 +1,6 @@
 import { publicProcedure, router } from '../trcp'
 import { deckRouter } from './deck'
-import { flashcardRouter } from './flashcard'
+import { flashcardRouter } from './flashcard1'
 import { habitRouter } from './habit'
 import { userRouter } from './user'
 
