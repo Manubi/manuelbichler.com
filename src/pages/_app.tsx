087a2e03d947b89760b01920c4bf6dab41fa5068
Manuel Bichler <mbichler@cemm.at>
2023-03-09 10:03:43 +0100

    changed back index

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -9,9 +9,12 @@ import {
 
 import type { AppProps } from 'next/app'
 
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
 import '@/styles/tailwind.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import 'focus-visible'
+import { Toaster } from 'react-hot-toast'
 import { trpc } from '../utils/trpc'
 
 function usePrevious(value) {
@@ -47,18 +50,17 @@ function MyApp({
       <QueryClientProvider client={queryClient}>
         <Hydrate state={pageProps.dehydratedState}>
           <div className="fixed inset-0 flex justify-center sm:px-8">
-            {/* <div className="flex w-full max-w-7xl lg:px-8">
+            <div className="flex w-full max-w-7xl lg:px-8">
               <div className="w-full bg-white ring-1 ring-zinc-100 dark:bg-zinc-900 dark:ring-zinc-300/20" />
-            </div> */}
+            </div>
           </div>
-          <div className="relative w-full text-center h-100 mt-60">
-            <p>coming soonish</p>
-            {/* <Header />
+          <div className="relative">
+            <Header />
             <main>
               <Component previousPathname={previousPathname} {...pageProps} />
             </main>
             <Footer />
-            <Toaster /> */}
+            <Toaster />
           </div>
         </Hydrate>
         <ReactQueryDevtools initialIsOpen={false} />
